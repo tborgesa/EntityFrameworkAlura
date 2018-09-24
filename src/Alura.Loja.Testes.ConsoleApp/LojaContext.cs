70a3d1dbb463bc57f5aef62ece49d747056517fd
Thiago Borges <tborgesa@gmail.com>
2018-09-23 22:09:58 -0300

    Aula 01.08 - Fazendo o mesmo insert usando o Entity

---
+++ src/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-LBQFR0N;Database=LojaDB;Trusted_Connection=true;");
+        }
+
+    }
+}
--- src/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ src/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,7 +10,35 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            //GravarUsandoAdoNet();
+            GravarUsandoEntity();
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Produtos.Add(p);
+                contexto.SaveChanges();
+                /*
+                exec sp_executesql N'SET NOCOUNT ON;
+                    INSERT INTO [Produtos] ([Categoria], [Nome], [Preco])
+                    VALUES (@p0, @p1, @p2);
+                    SELECT [Id]
+                    FROM [Produtos]
+                    WHERE @@ROWCOUNT = 1 AND [Id] = scope_identity();
+
+                    ',N'@p0 nvarchar(4000),@p1 nvarchar(4000),@p2 float',
+                    @p0=N'Livros',
+                    @p1=N'Harry Potter e a Ordem da Fênix',
+                    @p2=19,890000000000001 
+                */
+            }
         }
 
         private static void GravarUsandoAdoNet()
@@ -23,6 +51,13 @@ namespace Alura.Loja.Testes.ConsoleApp
             using (var repo = new ProdutoDAO())
             {
                 repo.Adicionar(p);
+                /*
+                 exec sp_executesql N'INSERT INTO Produtos (Nome, Categoria, Preco) VALUES (@nome, @categoria, @preco)',
+                 N'@nome nvarchar(31),@categoria nvarchar(6),@preco float',
+                 @nome=N'Harry Potter e a Ordem da Fênix',
+                 @categoria=N'Livros',
+                 @preco=19,890000000000001
+                 */
             }
         }
     }
